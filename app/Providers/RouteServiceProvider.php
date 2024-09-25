23b1e7b442faca891db223e09a7eca68c7930a61
Meheady <64876349+Meheady@users.noreply.github.com>
2024-09-25 22:17:12 +0600

    create customer route for separate route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+            Route::prefix('admin')->middleware('admin')
+                ->group(base_path('routes/admin.php'));
+            Route::prefix('customer')->middleware('customer')
+                ->group(base_path('routes/customer.php'));
         });
     }
 }
